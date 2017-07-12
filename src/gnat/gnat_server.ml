ea90541b1efaa6e2d438fd0ae6f06eedfc3dc48e
Sylvain Dailler <dailler@adacore.com>
2017-07-12 13:23:47 +0200

    Adapt to change for focusing
    Change-Id: Ia225bec26baf4d9a8cd4034ca6662725e737f706

--- src/gnat/gnat_server.ml
+++ src/gnat/gnat_server.ml
@@ -181,7 +181,7 @@ let () =
         (check.Gnat_expl.reason = Gnat_expl.get_reason expl)
         && (Gnat_loc.equal_orig_loc check.Gnat_expl.sloc (Gnat_expl.get_loc expl))
       in
-      Server.register_label_detection f
+      Server.focus_on_loading f
   (* TODO None and _ cases should be exit with errors *)
   | None -> ()
   | _ -> ()
