724d2340aa33658d27438cd6caab71e2a9972348
Johannes Kanig <kanig@adacore.com>
2023-11-30 09:40:19 +0900

    fixes

--- fsf_build.py
+++ fsf_build.py
@@ -1,23 +1,8 @@
+import argparse
 import os
 import os.path
 import subprocess
 import sys
-
-cwd = os.getcwd()
-
-target_dir = os.path.join(cwd, "staging")
-
-def print_command(args):
-    for arg in args:
-        print(arg, end=" ")
-    print("")
-    sys.stdout.flush()
-
-
-def run(args):
-    print_command(args)
-    subprocess.run(args, shell=True)
-
 
 depext = ["zarith", "re", "seq", "why3"]
 install = [
@@ -35,19 +20,60 @@ install = [
     "ppx_deriving",
 ]
 
-run(["opam", "depext"] + depext)
-run(["opam", "install"] + install)
+configure_opts = [
+    "--enable-relocation",
+    "--disable-emacs-compilation",
+    "--disable-hypothesis-selection",
+    "--disable-js-of-ocaml",
+    "--disable-zip",
+]
 
-run(
-    [
-        os.path.join(cwd, "configure"),
-        "--prefix=" + target_dir,
-        "--enable-relocation",
-        "--disable-emacs-compilation",
-        "--disable-hypothesis-selection",
-        "--disable-js-of-ocaml",
-        "--disable-zip",
-    ]
-)
-run(["make"])
-run(["make", "install_spark2014"])
+
+def parseargs():
+    parser = argparse.ArgumentParser(
+        prog="fsf_build.py",
+        description="Script to build why3 for SPARK",
+    )
+    parser.add_argument(
+        "--prefix",
+        dest="prefix",
+        help="target dir for the install",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def print_command(args):
+    for arg in args:
+        print(arg, end=" ")
+    print("")
+    sys.stdout.flush()
+
+
+def run(args):
+    print_command(args)
+    p = subprocess.run(args, shell=True)
+    p.check_returncode()
+
+
+def compute_targetdir(prefix=None):
+    if not prefix:
+        prefix = "staging"
+    return os.path.abspath(prefix)
+
+
+def main():
+    args = parseargs()
+    run(["opam", "depext"] + depext)
+    run(["opam", "install"] + install)
+    run(
+        [
+            "./configure",
+            "--prefix=" + compute_targetdir(args.prefix),
+        ]
+        + configure_opts
+    )
+    run(["make"])
+    run(["make", "install_spark2014"])
+
+main()
